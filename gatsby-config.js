bb64b1e8127930b59feb40806d7ff25f5c5c8ad1
amandayyang15 <amandayyang15@gmail.com>
2024-09-08 19:23:50 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Too Many Equations`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-sitemap"]
+};
