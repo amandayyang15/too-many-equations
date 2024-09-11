5dce55669cf238bfa6fbaff1e56cb6378c8bb7a3
amandayyang15 <amandayyang15@gmail.com>
2024-09-11 15:15:52 -0700

    linked up input-handling w calculation

--- src/logic/calculation.js
+++ src/logic/calculation.js
@@ -1,18 +1,8 @@
 import * as math from "mathjs";
+import InputHandling from "./input-handling";
 
 export default function SolveEquations(equations, variables) {
-    const eqns = equations + ''.split("\n").filter(Boolean); //.filter(Boolean) removes empty strings from array
-    const vars = variables + ''.split("\n").filter(Boolean);
-
-    console.log("Equations:", eqns);
+    console.log("Equations:", equations);
 
     return "eqns";
-
-    // try {
-    //     const solution = math.lusolve(eqns, vars);
-    //     return solution;
-    // } catch (error) {
-    //     console.error("Error solving equations:", error);
-    //     return "Error solving equations. Please check your input.";
-    // }
 };
--- src/logic/input-handling.js
+++ src/logic/input-handling.js
@@ -1,13 +1,19 @@
+import SolveEquations from "./calculation";
+
 //check for invalid inputs, then do symbolic expression transformations
 export default function InputHandling(equations, variables) {
     const eqns = equations + ''.split("\n").filter(Boolean); //.filter(Boolean) removes empty strings from array
     const vars = variables + ''.split("\n").filter(Boolean);
 
-    const invalidInput = InvalidInput(eqns, vars);
+    const invalidInput = InvalidInputCheck(eqns, vars);
+
+    if (invalidInput){
+        SolveEquations(eqns, vars);
+    }
 }
 
 //error handling for inputs
-function InvalidInput(eqns, vars){
+function InvalidInputCheck(eqns, vars){
     const validEquationsRegex = /^[0-9a-zA-Z\s\+\-\*\/=\(\)]*$/;
     const validVariablesRegex = /^[a-zA-]*$/;
 
@@ -18,13 +24,19 @@ function InvalidInput(eqns, vars){
 
     if (!validEquationsRegex.test(eqns)) {
         console.error("Invalid character(s) detected in the equation.");
-        return "Invalid character(s) detected in the equation.";
+        return "Invalid character(s) detected in the equation. Numbers and symbols are not allowed";
     }
 
     if (!validVariablesRegex.test(vars)) {
         console.error("Invalid character(s) detected in the variable field.");
         return "Invalid character(s) detected in the variable field.";
     }
+
+    else {
+        return true;
+    }
+    //if equation letters contain only those that exist in variables
+    //that each variable is unique and none is a part of another variable (ex a and Va)
 }
 
 //convert symbolic equations to mathjs lusolve compatible format
